2be298764b0e66683ef5a7f0aa51471def91e66d
sorabhSofttrix <sorabhsofttrix@gmail.com>
2020-02-28 12:38:27 +0530

    shift select

--- src/app/api-services/api-service/api-service.service.ts
+++ src/app/api-services/api-service/api-service.service.ts
@@ -13,7 +13,7 @@ import { switchMap, catchError } from 'rxjs/operators';
 })
 export class ApiServiceService {
   // baseUrl: string = 'https://6a4db3b3.ngrok.io/';
-  baseUrl: string = 'http://941d993f.ngrok.io/';
+  baseUrl: string = 'http://localhost:8000/';
 
 
   constructor(private http: HttpClient) { }
